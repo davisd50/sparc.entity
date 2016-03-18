1499d5639760dbf382707f3d432bc49af3c3f568
David Davis <davisd50@gmail.com>
2016-03-17 22:03:41 -0400

    update for test layer

--- sparc/entity/__init__.py
+++ sparc/entity/__init__.py
@@ -7,10 +7,3 @@ from interfaces import IUrlReference
 from interfaces import IKeyphraseTags
 
 from entity import SparcEntity
-
-# Configuration (this package only)
-from importlib import import_module
-from sparc.configuration.zcml import Configure as SparcConfigure
-def Configure():
-    SparcConfigure([import_module(__name__),
-                    import_module('zope.annotation')])
--- sparc/entity/persistent/tests/test_entity.py
+++ sparc/entity/persistent/tests/test_entity.py
@@ -1,13 +1,11 @@
 import unittest
-from doctest import DocTestSuite
-from doctest import DocFileSuite
+from sparc.testing.fixture import test_suite_mixin
 
-import sparc.entity.persistent
 
-def test_suite():
-    return unittest.TestSuite((
-        DocFileSuite('entity.txt',
-                     package=sparc.entity.persistent),))
+class test_suite(test_suite_mixin):
+    package = 'sparc.entity'
+    module = 'entity'
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
